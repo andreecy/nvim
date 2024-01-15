6d92d041c24ffe5a6bc192a0f4ae8166835eb68d
andreecy <andreechristianyulianto@gmail.com>
2024-01-15 21:47:45 +0700

    add yanky

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- local lazyvim_util = require("lazyvim.util")
+local set = vim.keymap.set
+local del = vim.keymap.del
+
+-- remove windows default keymap
+del("n", "<leader>ww")
+del("n", "<leader>wd")
+del("n", "<leader>w-")
+del("n", "<leader>w|")
+
+set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save current buffer" })
