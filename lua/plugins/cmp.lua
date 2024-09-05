9d740613a5f50e913f9c1f9b780f913673365b65
andreecy <andreechristianyulianto@gmail.com>
2024-09-05 22:07:58 +0700

    supermaven completion using tab

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -25,18 +25,22 @@ return {
       end
 
       local cmp = require("cmp")
+      local suggestion = require("supermaven-nvim.completion_preview")
 
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
         ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
+          if suggestion.has_suggestion() then
+            suggestion.on_accept_suggestion()
+          elseif cmp.visible() then
             -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
             cmp.select_next_item()
+            -- cmp.confirm({ select = true })
           elseif vim.snippet.active({ direction = 1 }) then
             vim.schedule(function()
               vim.snippet.jump(1)
             end)
-          elseif has_words_before() then
-            cmp.complete()
+          -- elseif has_words_before() then
+          --   cmp.complete()
           else
             fallback()
           end
